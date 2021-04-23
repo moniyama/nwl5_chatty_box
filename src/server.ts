1c11d5db43e7ed9a6b2f5dd55fc3aa708ba35118
Monica <monicasayama@gmail.com>
2021-04-23 17:11:15 -0300

    add sockeio conection

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,22 @@ import express from "express";
 // import "reflect-metadata";
 import "./database"
 import { routes } from "./routes"
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
+const PORT = 3333
 const app = express();
 app.use(express.json())
 
-const PORT = 3333
+const httpServer = createServer(app)
+const io = new Server(httpServer);
+
+io.on("connection", (socket: Socket) => {
+  // ...
+});
+
+io.listen(3000);
+
 app.use(routes)
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+httpServer.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
