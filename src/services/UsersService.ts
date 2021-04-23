af6b44c0d39a765a1426ccf5775903c0d2e8b413
Monica <monicasayama@gmail.com>
2021-04-22 22:36:39 -0300

    add users service

---
+++ src/services/UsersService.ts
@@ -0,0 +1,23 @@
+import { UsersRepository } from "../repositories/UsersRepository"
+import { getCustomRepository } from "typeorm"
+
+
+
+class UsersService {
+  async create(email:string){
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userExists = await usersRepository.findOne({email})
+    if(userExists){
+      return userExists;
+    }
+  
+    const user = usersRepository.create({ email })
+
+    await usersRepository.save(user)
+    
+    return user
+  }
+}
+
+export { UsersService }
