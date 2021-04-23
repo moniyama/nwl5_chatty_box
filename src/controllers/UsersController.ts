cbe437aa736ae824e0b7fa0d8c09f1bc68f4348e
Monica <monicasayama@gmail.com>
2021-04-22 22:44:51 -0300

    add users controller e routes

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express"
+import { UsersService } from "../services/UsersService"
+
+class UsersController {
+  async create(req: Request, res: Response): Promise<Response> {
+    const { email } = req.body
+
+    const usersService = new UsersService()
+
+    try {
+      const user = await usersService.create(email)
+      return res.json(user)
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message
+      })
+    }
+  }
+}
+
+export { UsersController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import { SettingController } from "./controllers/SettingsController"
+import { UsersController } from "./controllers/UsersController"
 const routes = Router()
 
 const settingsController = new SettingController()
+const usersController = new UsersController()
+
 routes.post("/settings", settingsController.create)
+routes.post("/users", settingsController.create)
 
 export { routes }
