feb41bd8a1679a48aa9712a671acd300dce221e2
Monica <monicasayama@gmail.com>
2021-04-24 20:00:32 -0300

    add front-end

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,26 @@ import "./database"
 import { routes } from "./routes"
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import path from "path"
 
 const PORT = 3333
 const app = express();
-app.use(express.json())
+
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// //http://expressjs.com/en/5x/api.html#app.set
+app.set("views", path.join(__dirname, '..', 'public'))
+
+// //http://expressjs.com/en/5x/api.html#app.engine
+app.engine('html', require('ejs').renderFile)
+
+app.set("view engine", "html")
+
+app.get("/pages/client", (req, res) => {
+  console.log('entrou')
+  return res.render('html/client.html')
+})
 
 const httpServer = createServer(app)
 const io = new Server(httpServer);
@@ -16,7 +32,7 @@ io.on("connection", (socket: Socket) => {
   // ...
 });
 
-io.listen(3000);
+app.use(express.json())
 
 app.use(routes)
 
