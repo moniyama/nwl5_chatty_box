2a98e1f70a2f6c7f71c00478b5a9f9693db1dd00
Monica <monicasayama@gmail.com>
2021-04-24 22:44:16 -0300

    add update setting & fix findByUsername setting

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -21,7 +21,16 @@ class SettingController {
     const { username } = req.params
 
     const settingsService = new SettingsService()
-    const settings = settingsService.findByUsername(username)
+    const settings = await settingsService.findByUsername(username)
+    return res.json(settings)
+  }
+
+  async update(req: Request, res: Response){
+    const { username } = req.params
+    const { chat } = req.body
+
+    const settingsService = new SettingsService()
+    const settings = await settingsService.update(username, chat)
     return res.json(settings)
   }
 }
