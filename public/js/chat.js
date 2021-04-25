354880e934cdcbe270a332fac59a614c033973ea
Monica <monicasayama@gmail.com>
2021-04-25 02:56:45 -0300

    add admin chat updating when get response

--- public/js/chat.js
+++ public/js/chat.js
@@ -63,7 +63,7 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
 document.querySelector("#send_message_button").addEventListener("click", (event) => {
   const text= document.getElementById("message_user")
   const params = {
-    text, 
+    text: text.value, 
     socket_admin_id
   }
 
